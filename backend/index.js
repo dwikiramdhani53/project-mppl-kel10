259c249f85b388243cc3a18d6917d3f3646d7e05
linggar <linggar_asmara@apps.ipb.ac.id>
2021-12-01 23:57:53 +0700

    Update Index.Js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const { verifyToken } = require('./helper/jwt')
 const users = require('./class/users')
 const produk = require('./class/produk')
-const Article = require('./class/Article')
+const ArticleRoutes = require('./routes/ArticleRoutes')
 const ServiceRoutes = require('./routes/ServiceRoutes')
-const Community = require('./class/Community')
+const CommunityRoutes = require('./routes/CommunityRoutes')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -23,19 +23,9 @@ app.get('/products', produk.getAll)
 app.get('/product', produk.getOne)
 app.post('/insertProduct', produk.addProduk)
 
-app.get('/AllArticle',Article.getAllArticle)
-app.get('/AllArticleByAdmin',Article.getAllArticleByAdmin)
-app.post('/OneArticle',Article.getOneArticle)
-app.post('/AddArticle',Article.addArticle)
-app.post('/EditArticle',Article.editArticle)
-app.post('/DeleteArticle',Article.deleteArticle)
-
+app.use('/Article',ArticleRoutes)
 app.use('/Services',ServiceRoutes)
-
-app.get('/AllCommunity',Community.getAllCommunity)
-app.post('/OneCommunity',Community.getOneCommunity)
-app.post('/AddCommunity',Community.addCommunity)
-app.post('/DeleteCommunity',Community.deleteCommunity)
+app.use('/Community',CommunityRoutes)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
